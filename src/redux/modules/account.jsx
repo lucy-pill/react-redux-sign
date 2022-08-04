da464dd9b13084f726d42ed34809ce5ae4cc5b05
lucy-pill <schmidtiana95@gmail.com>
2022-08-05 01:45:42 +0900

    Migration to @reduxjs/toolkit

--- src/redux/modules/account.jsx
+++ src/redux/modules/account.jsx
@@ -1,46 +1,75 @@
 import { db } from '../../utils/firebase';
 import { collection, addDoc } from 'firebase/firestore';
+import { createSlice } from '@reduxjs/toolkit';
 
-const SIGNUP = 'account/SIGNUP';
-const SIGNIN = 'account/SIGNIN';
-const SIGNOUT = 'account/SIGNOUT';
-
-export const signUpAction = (account) => {
-  return { type: SIGNUP, account };
-};
-export const signInAction = (signInInfo) => {
-  return { type: SIGNIN, signInInfo };
-};
-export const signOutAction = (signOutResult) => {
-  return { type: SIGNOUT, signOutResult };
-};
-
-const initialState = {
-  userName: null,
-  loginStatus: false,
-};
-
-export default function reducer(state = initialState, action = {}) {
-  switch (action.type) {
-    case 'account/SIGNUP': {
+const signSlice = createSlice({
+  name: 'sign',
+  initialState: {
+    userName: null,
+    loginStatus: false,
+  },
+  reducers: {
+    signUpAction: (state, action) => {
       try {
-        addDoc(collection(db, 'account'), action.account);
+        addDoc(collection(db, 'account'), action.payload);
       } catch (error) {
         console.error(`Error adding document: ${error}`);
       }
-      return state;
-    }
-    case 'account/SIGNIN': {
-      console.log(action.signInInfo);
-      return {
-        userName: action.signInInfo.userName,
-        loginStatus: action.signInInfo.loginStatus,
-      };
-    }
-    case 'account/SIGNOUT': {
-      return { userName: null, loginStatus: action.signOutResult };
-    }
-    default:
-      return state;
-  }
-}
+    },
+    signInAction: (state, action) => {
+      state.userName = action.payload.userName;
+      state.loginStatus = action.payload.loginStatus;
+    },
+    signOutAction: (state, action) => {
+      state.userName = null;
+      state.loginStatus = action.payload;
+    },
+  },
+});
+ 
+export const {signUpAction, signInAction, signOutAction} = signSlice.actions;
+export default signSlice.reducer;
+
+// const SIGNUP = 'account/SIGNUP';
+// const SIGNIN = 'account/SIGNIN';
+// const SIGNOUT = 'account/SIGNOUT';
+
+// export const signUpAction = (account) => {
+//   return { type: SIGNUP, account };
+// };
+// export const signInAction = (signInInfo) => {
+//   return { type: SIGNIN, signInInfo };
+// };
+// export const signOutAction = (signOutResult) => {
+//   return { type: SIGNOUT, signOutResult };
+// };
+
+// const initialState = {
+//   userName: null,
+//   loginStatus: false,
+// };
+
+// export default function reducer(state = initialState, action = {}) {
+//   switch (action.type) {
+//     case 'account/SIGNUP': {
+//       try {
+//         addDoc(collection(db, 'account'), action.account);
+//       } catch (error) {
+//         console.error(`Error adding document: ${error}`);
+//       }
+//       return state;
+//     }
+//     case 'account/SIGNIN': {
+//       console.log(action.signInInfo);
+//       return {
+//         userName: action.signInInfo.userName,
+//         loginStatus: action.signInInfo.loginStatus,
+//       };
+//     }
+//     case 'account/SIGNOUT': {
+//       return { userName: null, loginStatus: action.signOutResult };
+//     }
+//     default:
+//       return state;
+//   }
+// }
